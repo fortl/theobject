79cb252721c2965c3dfb8ce436dba81d3d571624
Alexander Urbanovich <au@time.sc>
2020-02-29 14:48:20 +0300

    + constants * new protocol version + watchdog

--- spark-attiny2313/spark-attiny2313.ino
+++ spark-attiny2313/spark-attiny2313.ino
@@ -1,15 +1,16 @@
 #include <avr/wdt.h>
 
-#define ADDRESS 15
+#define ADDRESS 14
 
-#define LED 11
+#define LED_PIN 11
+#define SET_PIN 7
+#define TXD_PIN 1
 //#define ENABLE_UART_DEBUG
 
-#define TOTAL_COUNT 20
-#define SUMM_BYTES 10
-#define SUMM_BYTE_NUMBER (byte)(ADDRESS*SUMM_BYTES/TOTAL_COUNT)
-#define SUMM_START_BYTE (SUMM_BYTE_NUMBER*TOTAL_COUNT/SUMM_BYTES)
-#define BUFFER_SIZE (TOTAL_COUNT+SUMM_BYTES+2)
+#define GROUP_SIZE 4
+#define BUFFER_SIZE 1+GROUP_SIZE+1 // groupId|summ + leds + null
+#define GROUP_ID (byte)(ADDRESS/GROUP_SIZE)
+#define LOCAL_GROUP_ADDRESS (ADDRESS % GROUP_SIZE)
 
 uint8_t buffer[BUFFER_SIZE+1]; // command plus terminating null
 char bufferIndex = 0;
@@ -21,9 +22,16 @@ unsigned char USART_Receive( void );
 void USART_Transmit( unsigned char data );
  
 void setup() {
-  pinMode(LED, OUTPUT);
+  pinMode(LED_PIN, OUTPUT);
+  pinMode(SET_PIN, OUTPUT);
+  digitalWrite(SET_PIN, LOW);
+#ifndef ENABLE_UART_DEBUG  
+  pinMode(TXD_PIN, OUTPUT);
+  digitalWrite(TXD_PIN, HIGH);  
+#endif 
+  TCCR0B = (1 << CS00); 
   USART_Init( 77 ); 
-  wdt_enable(WDTO_1S);
+  wdt_enable(WDTO_2S);
 }
 
 void loop() {
@@ -31,16 +39,16 @@ void loop() {
   uint8_t c = USART_Receive(); 
   buffer[bufferIndex++] = c;
   if ((c == '\0') || (bufferIndex == BUFFER_SIZE)) {
-    if( (buffer[ADDRESS] > 0) && (bufferIndex > ADDRESS)) {
-      value = buffer[ADDRESS]-1;
+    if( (bufferIndex == BUFFER_SIZE) && (((buffer[0] >> 4) & 0x07) == GROUP_ID) ) {
+      value = buffer[LOCAL_GROUP_ADDRESS+1]-1;
 #ifdef ENABLE_UART_DEBUG  
-      USART_Transmit(buffer[ADDRESS]);
+      USART_Transmit(value);
 #endif 
       uint8_t summ = 0;
-      for (uint8_t i = 0; i < (TOTAL_COUNT/SUMM_BYTES); i++)
-        summ ^= buffer[SUMM_START_BYTE + i];
-      if( (summ|1) == buffer[TOTAL_COUNT+SUMM_BYTE_NUMBER] )
-       analogWrite(LED, value);
+      for (uint8_t i = 0; i < GROUP_SIZE; i++)
+        summ ^= buffer[1 + i];
+      if( ((summ & 0x0f) ^ (summ >> 4)) == (buffer[0] & 0x0f) )
+       analogWrite(LED_PIN, value);
     }
     bufferIndex = 0;
   }
@@ -58,11 +66,11 @@ void USART_Init( unsigned int baudrate ) {
 }
 
 unsigned char USART_Receive( void ) {
-  while ( !(UCSRA & (1<<RXC)) );  
+  while ( !(UCSRA & (1<<RXC)) ) wdt_reset();
   return UDR;
 }
  
 void USART_Transmit( unsigned char data ) {
-  while ( !(UCSRA & (1<<UDRE)) ); 
+  while ( !(UCSRA & (1<<UDRE)) ) wdt_reset(); 
   UDR = data;         
 }
