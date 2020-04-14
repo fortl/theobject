43a34a65aeaee26852375ee91c682264d1ff6758
Alexander Urbanovich <au@time.sc>
2020-04-15 00:30:59 +0300

    - remove old style code

--- gate/olaplugin/effects/shadow.py
+++
@@ -1,9 +0,0 @@
-from olaplugin.effects.flash_random import FlashRandomEffect
-
-import math
-
-class ShadowEffect(FlashRandomEffect):
-    def set_data_unit(self, data, unit: int):
-        value = int(math.sqrt(self.values[unit]*self.brightness.get_value()))
-        if value > 0:
-            data[unit] = data[unit]/value
