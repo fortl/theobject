2e601777797cbf336124da705eb6f20f3bef05fc
Alexander Urbanovich <au@time.sc>
2020-06-10 23:57:02 +0300

    - debug

--- gate/olaplugin/routes.py
+++ gate/olaplugin/routes.py
@@ -5,5 +5,4 @@ def setup_routes(app):
     app.router.add_get('/channel', v.set_channel)
     app.router.add_get('/wifi', v.wifi, name='wifi')
     app.router.add_get('/touchosc', v.touchosc)
-    app.router.add_get()
     
